0e42c7544657385bcf1e40c4641371df05f7c315
Cristales <mariocardona0501@gmail.com>
2024-05-05 20:19:57 -0600

    Proyecto 2

--- multiagent/multiAgents.py
+++ multiagent/multiAgents.py
@@ -75,7 +75,6 @@ class ReflexAgent(Agent):
         newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
 
         "*** YOUR CODE HERE ***"
-        
         newFood = newFood.asList()
         nearestF = 999999999
 
@@ -90,7 +89,7 @@ class ReflexAgent(Agent):
             dist = manhattanDistance(newPos, g)
             if (dist < 2): #if the ghost is too close then abort
                 return -nearestF
-        return (successorGameState.getScore()*nearestF + math.exp(1))/nearestF 
+        return (successorGameState.getScore()*nearestF + math.exp(1))/nearestF #else update the evaluation function
 
 def scoreEvaluationFunction(currentGameState):
     """
@@ -145,7 +144,49 @@ class MinimaxAgent(MultiAgentSearchAgent):
             Returns the total number of agents in the game
         """
         "*** YOUR CODE HERE ***"
+        #initialization
+        return self.maximum(gameState, 0, 0)[0]
         util.raiseNotDefined()
+
+
+    def minimaxAgent(self, gameState, aIndex, d):
+        won = gameState.isWin() #check if the game is won
+        lost = gameState.isLose() #check if the game is lost
+        noAgents = self.depth*gameState.getNumAgents()
+        if d is noAgents or won or lost:
+            return self.evaluationFunction(gameState) #if the game is won or lost call the evaluation function
+
+        #for odd depth iterations minimize else maximize
+        var = self.minimum(gameState, aIndex, d)[1] if aIndex != 0 else self.maximum(gameState, aIndex, d)[1]
+        return var
+
+    def maximum(self, gameState, aIndex, d):
+        legalActions = gameState.getLegalActions(aIndex)
+        bestChoice = ("max", -999999999)
+        d = d + 1
+        #finds the best max reward choice among the given choices to choose from
+        for act in legalActions:
+            successor = gameState.generateSuccessor(aIndex, act)
+            noAgents = d%gameState.getNumAgents()
+            minimax = self.minimaxAgent(successor, noAgents, d) #calculates the best move
+            successorMove = (act, minimax)
+            key = lambda x: x[1]
+            bestChoice = max(bestChoice, successorMove, key=key) #compares the new founded move to the older best
+        return bestChoice
+
+    def minimum(self, gameState, aIndex, d):
+        legalActions = gameState.getLegalActions(aIndex)
+        bestChoice = ("min", 999999999)
+        d = d+1
+        # finds the best min reward choice among the given choices to choose from (opponents turn)
+        for act in legalActions:
+            successor = gameState.generateSuccessor(aIndex, act)
+            noAgents = d%gameState.getNumAgents()
+            minimax = self.minimaxAgent(successor, noAgents, d) #calculates the best move
+            successorMove = (act, minimax)
+            key = lambda x: x[1]
+            bestChoice = min(bestChoice, successorMove, key=key) #compares the new founded move to the older best
+        return bestChoice
 
 class AlphaBetaAgent(MultiAgentSearchAgent):
     """
@@ -157,7 +198,56 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
           Returns the minimax action using self.depth and self.evaluationFunction
         """
         "*** YOUR CODE HERE ***"
+        #initialization
+        num = 999999999
+        max = self.maximum(gameState,0,0,-num,num)[0]
+        return max
         util.raiseNotDefined()
+
+    def abPruning(self, gameState, aIndex, d, a, b):
+        won = gameState.isWin()
+        lost = gameState.isLose()
+        noAgents = self.depth*gameState.getNumAgents()
+        if d is noAgents or won or lost:
+            return self.evaluationFunction(gameState) #if the game is won or lost call the evaluation function
+
+        # for odd depth iterations minimize else maximize
+        var = self.minimum(gameState, aIndex, d, a, b)[1] if aIndex != 0 else self.maximum(gameState, aIndex, d, a, b)[1]
+        return var
+
+    def maximum(self, gameState, aIndex, d, a, b):
+        legalActions = gameState.getLegalActions(aIndex)
+        bestChoice = ("max", -999999999)
+        d=d+1
+        for act in legalActions:
+            successor = gameState.generateSuccessor(aIndex, act)
+            noAgents = d%gameState.getNumAgents()
+            ab = self.abPruning(successor, noAgents, d, a, b) #calculates the best move
+            successorMove = (act, ab)
+            key = lambda x: x[1]
+            bestChoice = max(bestChoice, successorMove, key=key) #compares the new founded move to the older best
+            if bestChoice[1] > b: #pruning with respect to the beta value
+                return bestChoice
+            else:
+                a = max(a, bestChoice[1])
+        return bestChoice
+
+    def minimum(self, gameState, aIndex, d, a, b):
+        legalActions = gameState.getLegalActions(aIndex)
+        bestChoice = ("min", 999999999)
+        d = d + 1
+        for act in legalActions:
+            successor = gameState.generateSuccessor(aIndex, act)
+            noAgents = d%gameState.getNumAgents()
+            ab = self.abPruning(successor, noAgents, d, a, b) #calculates the best move
+            successorMove = (act, ab)
+            key = lambda x: x[1]
+            bestChoice = min(bestChoice, successorMove, key=key) #compares the new founded move to the older best
+            if bestChoice[1] < a: #pruning with respect to the alpha value
+                return bestChoice
+            else:
+                b = min(b, bestChoice[1])
+        return bestChoice
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
     """
@@ -172,7 +262,44 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
           legal moves.
         """
         "*** YOUR CODE HERE ***"
+        d = self.depth* gameState.getNumAgents()
+        expecti = self.expectimaxAlgo(gameState,"expect", d, 0)[0]
+        return expecti
         util.raiseNotDefined()
+
+    def expectimaxAlgo(self, gameState, act, d, aIndex):
+        won = gameState.isWin()
+        lost = gameState.isLose()
+        if d is 0 or lost or won:
+            return act, self.evaluationFunction(gameState) #if the game is won or lost call the evaluation function
+
+        # for odd depth iterations minimize else maximize
+        var = self.maximum(gameState, act, d, aIndex) if aIndex == 0 else self.expected(gameState, act, d, aIndex)
+        return var
+
+    def maximum(self, gameState, act, d, aIndex):
+        legalActions = gameState.getLegalActions(aIndex)
+        bestChoice = ("max", -999999999)
+        for a in legalActions:
+            newAgent = (aIndex+1)%gameState.getNumAgents()
+            ndepth = self.depth*gameState.getNumAgents()
+            successor = a if d==ndepth else act
+            s = gameState.generateSuccessor(aIndex, a)
+            successorVal = self.expectimaxAlgo(s,successor, d-1, newAgent) #calculates the best move
+            k = lambda x: x[1]
+            bestChoice = max(bestChoice, successorVal, key=k) #compares the new founded move to the older best
+        return bestChoice
+
+    def expected(self, gameState, act, d, aIndex):
+        legalActions = gameState.getLegalActions(aIndex)
+        avg = 0
+        p = 1.0/len(legalActions) #probability calc
+        for a in legalActions:
+            newAgent = (aIndex+1)%gameState.getNumAgents()
+            s = gameState.generateSuccessor(aIndex, a) #generate a new successor
+            bestChoice = self.expectimaxAlgo(s, act, d-1, newAgent) #finds the best move
+            avg += bestChoice[1]*p #calculates average
+        return act, avg
 
 def betterEvaluationFunction(currentGameState):
     """
@@ -182,6 +309,27 @@ def betterEvaluationFunction(currentGameState):
       DESCRIPTION: <write something here so we know what you did>
     """
     "*** YOUR CODE HERE ***"
+    nextPosition = currentGameState.getPacmanPosition()
+    food = currentGameState.getFood().asList()
+    newFood = 999999999
+    for i in food:
+        newFood = min(newFood, manhattanDistance(nextPosition, i)) #find the closest food positon
+
+    ghostPosition = 0 #initialize the ghost position
+    ghostRoute = currentGameState.getGhostPositions()
+    for i in ghostRoute:
+        ghostPosition = manhattanDistance(nextPosition, i) #update ghost position
+        if (ghostPosition < 2): #if the ghost is too close abort
+            return -999999999
+    addFactors = 0
+    won = currentGameState.isWin()
+    lost = currentGameState.isLose()
+    if won:
+        addFactors += 10000 #add bias
+    elif lost:
+        addFactors -= 10000 #subtract bias
+    #updated evaluation function taking receprocals of the values at hand like the food position, ghost position and the capsules remaining in game along with the bias
+    return 20000 / (currentGameState.getNumFood() + math.exp(1)) + ghostPosition + 200 / (newFood + math.exp(1)) + 2000 / (len(currentGameState.getCapsules()) + math.exp(1)) + addFactors
     util.raiseNotDefined()
 
 # Abbreviation
